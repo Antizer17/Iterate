170ff4a51f4c70540837a76d6451fa08d0de8c42
Sameer <ahmad.sameer.5122@gmail.com>
2026-07-09 00:53:36 +0600

    feat: implement controller to handle still confused button

--- server/Routes/streakRoutes.js
+++ server/Routes/streakRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import syncStreak from '../controllers/streakController.js';
+import syncConfusion from '../controllers/confusedController.js'
 
 const router = express.Router();
 router.get("/sync", syncStreak);
+router.get("/confused", syncConfusion)
 
 export default router;
