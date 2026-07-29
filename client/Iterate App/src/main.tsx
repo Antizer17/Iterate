54e1e983c8b0619859cf08d2bf0a7cdd00145e2c
Sameer <ahmad.sameer.5122@gmail.com>
2026-07-30 01:15:13 +0600

    vercel deploy

--- client/Iterate App/src/main.tsx
+++ client/Iterate App/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './iterate-app.jsx'
-import {BrowserRouter, Link, useNavigate} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import {AuthProvider} from '../src/context/AuthContext.jsx'
 
 
