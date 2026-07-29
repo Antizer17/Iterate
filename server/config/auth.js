fe0dc13a717cf6ff86ee5e51b5a7660753786864
Sameer <ahmad.sameer.5122@gmail.com>
2026-07-30 02:00:07 +0600

    fix(urL): fix redirect url

--- server/config/auth.js
+++ server/config/auth.js
@@ -5,7 +5,7 @@ import User from '../models/users.js'; // Your MongoDB User Schema
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:1700/api/auth/google/callback"
+    callbackURL: "http://iterate-gy7v.onrender.com/api/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
