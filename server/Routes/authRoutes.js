d7f47012d68e87f956f14f50201f5aeccb8bc734
Sameer <ahmad.sameer.5122@gmail.com>
2026-08-01 13:36:09 +0600

    add logs for debug

--- server/Routes/authRoutes.js
+++ server/Routes/authRoutes.js
@@ -11,6 +11,8 @@ router.get('/google', passport.authenticate('google', { scope: ['profile', 'emai
 router.get('/google/callback', 
   passport.authenticate('google', { session: false, failureRedirect: '/login' }),
   (req, res) => {
+    console.log("✅ CALLBACK HIT");
+    console.log("User:", req.user?._id);
     // If we get here, authentication was successful!
     // Generate your own JWT session token using the authenticated req.user object
     const sessionToken = jwt.sign({ id: req.user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
@@ -22,6 +24,7 @@ router.get('/google/callback',
       sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000
     }); 
+    console.log("🍪 Cookie sent")
     res.redirect(`${process.env.CLIENT_URL}/courses`); 
   }
 );
